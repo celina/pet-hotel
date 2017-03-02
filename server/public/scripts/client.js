9c8c115d25574737a5fadc849d862aaab82f6708
Christine Pogatchnik <christinepogatchnik@gmail.com>
2017-03-02 17:04:53 -0600

    Owner Input

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,5 +5,25 @@ console.log('client.js is sourced');
 
 
 
-    
+    //#newOwnerForm event listener
+    $('#newOwnerForm').on('submit', function(event){
+      event.preventDefault();
+      var newOwnerObject = {};
+      var formFields = $(this).serializeArray();
+      formFields.forEach(function (field) {
+        newOwnerObject[field.name] = field.value;
+      });
+      $.ajax({
+        type: 'POST',
+        url: '/pets/new',
+        data: newOwnerObject,
+        success: function(response){
+          console.log(response);
+          getPetData();
+          $('#newOwnerForm > input').val('');
+        }
+      });// end ajax POST
+    });// end #newBookForm event listener
+
+
   }); // end document.ready
