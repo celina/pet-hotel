873ade1d0490569f21c29d3ef5af618a86c7056b
Celina Peterson <celinap@gmail.com>
2017-03-02 22:27:34 -0600

    removed form tags from index.html. put button event listeners inside document.ready. moved getPetData function to the bottom of client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,61 +2,30 @@ console.log('client.js is sourced');
   $(document).ready(function(){ // start document.ready
 
       getPetData();
-      function getPetData() {
-        $.ajax({
-          type: 'GET',
-          url: '/pets',
-          success: function(response) {
-            console.log('response', response); // response is an array of pet objects
-            // $('#newPetForm').empty(); // clears the pets in the #hotelTable
-            // ^^ celina: I think this was clearing and replacing the wrong element. I replaced it with tbody below:
-            $('#tbody').empty(); // clears the pets in the #hotelTable
-            for (var i = 0; i < response.length; i++) {
-              var currentPet = response[i]; // Loops through pets - This is an object
-              var $newPet = $('<tr>'); // Creating a new row for each pet
-              $newPet.data('id', currentPet.id);
-              $newPet.append('<td>'+ currentPet.owner_id + '</td>');
-              $newPet.append('<td>'+ currentPet.name + '</td>');
-              $newPet.append('<td>' + currentPet.breed + '</td>');
-              $newPet.append('<td>' + currentPet.color + '</td>');
-              $newPet.append('<td><button class="deleteButton">Delete</button></td>');
-              $newPet.append('<td><button class="saveButton">Update Pet</button></td>');
-              $('#hotelTable').append($newPet);
-            }
-          }
-        });  //closes ajax
 
+      // #registerButton event listener
+      $('#registerButton').on('click', function(event){
+        console.log('clicking new owner');
+        event.preventDefault();
+        var newOwnerObject = {};
+        var formFields = $(this).serializeArray();
+        formFields.forEach(function (field) {
+          newOwnerObject[field.name] =field.value;
+        });
+      }); // end #registerButton event listener
 
+      // #addPetButton event listener
+      $('#addPetButton').on('click', function(event){
+        console.log('clicking new pet');
+        event.preventDefault();
+        var newOwnerObject = {};
+        var formFields = $(this).serializeArray();
+        formFields.forEach(function (field) {
+          newOwnerObject[field.name] =field.value;
+        });
+      }); // end #addPetButton event listener
 
-
-
-    //
-    //     $.ajax({
-    //       type: 'POST',
-    //       url: '/pets/new',
-    //       data: newPetObject,
-    //       success: function(response){
-    //         console.log(response);
-    //         getPetData();
-    //         $('#newPetForm > input').val('');
-    //       }
-        };
-  });
-
-
-
-
-    //#newOwnerForm event listener
-    $('#newOwnerForm').on('submit', function(event){
-      console.log('clicking new owner');
-      event.preventDefault();
-      var newOwnerObject = {};
-      var formFields = $(this).serializeArray();
-      formFields.forEach(function (field) {
-        newOwnerObject[field.name] =field.value;
-      });
-
-    });// end #newBookForm event listener
+    }); // end document.ready
 
     // //#newPetForm event listener
     // $('#newPetForm').on('submit', function(event){
@@ -112,6 +81,27 @@ console.log('client.js is sourced');
     //   });// end ajax POST
     // });// end #newBookForm event listener
 
-
-
- // end document.ready
+    function getPetData() {
+      $.ajax({
+        type: 'GET',
+        url: '/pets',
+        success: function(response) {
+          console.log('response', response); // response is an array of pet objects
+          // $('#newPetForm').empty(); // clears the pets in the #hotelTable
+          // ^^ celina: I think this was clearing and replacing the wrong element. I replaced it with tbody below:
+          $('#tbody').empty(); // clears the pets in the #hotelTable
+          for (var i = 0; i < response.length; i++) {
+            var currentPet = response[i]; // Loops through pets - This is an object
+            var $newPet = $('<tr>'); // Creating a new row for each pet
+            $newPet.data('id', currentPet.id);
+            $newPet.append('<td>'+ currentPet.owner_id + '</td>');
+            $newPet.append('<td>'+ currentPet.name + '</td>');
+            $newPet.append('<td>' + currentPet.breed + '</td>');
+            $newPet.append('<td>' + currentPet.color + '</td>');
+            $newPet.append('<td><button class="deleteButton">Delete</button></td>');
+            $newPet.append('<td><button class="saveButton">Update Pet</button></td>');
+            $('#hotelTable').append($newPet);
+          }
+        }
+      });  //closes ajax
+    }; // end getPetData
